ae98cf91c590ce6a2844804d6ada9965108a5617
fromLittleAcorns <confusedjohn46@gmail.com>
2023-07-16 19:18:17 +0100

    prepared repro using nbdev

--- miniai/_modidx.py
+++ miniai/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://fromLittleAcorns.github.io',
                 'git_url': 'https://github.com/fromLittleAcorns/miniai',
                 'lib_path': 'miniai'},
-  'syms': {'miniai.core': {'miniai.core.foo': ('core.html#foo', 'miniai/core.py')}}}
+  'syms': { 'miniai.core': { 'miniai.core.foo': ('core.html#foo', 'miniai/core.py'),
+                             'miniai.core.say_hello': ('core.html#say_hello', 'miniai/core.py')}}}
